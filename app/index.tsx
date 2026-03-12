918bedf9233c8434b0c24e00d27f15b643876aba
MarcoCanha <1636625@hc.ac.je>
2026-03-12 12:08:13 +0000

    Hallpass first task added with checkbox
    it contains the basic styles to display a task

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,20 @@
-import * as React from 'react';
 import { View } from 'react-native';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Text } from '@/components/ui/text';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function HomeScreen() {
   return (
-    <View className="bg-background flex-1 items-center justify-center gap-5 p-6">
-      <Card className="w-full max-w-sm rounded-2xl p-6">
-        <CardHeader className="items-center">
-          <CardTitle variant="h1" className="text-brand-primary pb-2 text-center">
-            Hall Pass
-          </CardTitle>
-          <Text variant="muted" className="text-muted-foreground">
-            {new Date().toLocaleTimeString()}
-          </Text>
-        </CardHeader>
-
-        <CardContent>
-          <View className="flex-row justify-center gap-3">
-            <View className="items-center">
-              <Text className="text-muted-foreground text-sm">An app for</Text>
-              <CardTitle variant="h3" className="text-foreground font-semibold">
-                Students
-              </CardTitle>
-            </View>
-          </View>
-        </CardContent>
-
-        <CardFooter className="flex-col gap-3">
-          <View className="flex-row items-center overflow-hidden">
-            <Text variant="code">Update me with your own code</Text>
-          </View>
-        </CardFooter>
-      </Card>
+    <View className="bg-background flex flex-1 py-32">
+      <View className="text-foreground flex flex-row text-center">HallPass!</View>
+      <View className="border-foreground-transparent mx-50 flex flex-row gap-5 py-4">
+        <View className="flex items-center justify-center">
+          <Checkbox className="border-2"> </Checkbox>
+        </View>
+        <View className="border-foreground-transparent border-b">
+          <Text className="text-foreground">Submit Assignment</Text>
+          <Text className="text-foreground-transparent">Due: Oct 20</Text>
+        </View>
+      </View>
     </View>
   );
 }
