51f8069cbd9a18619f76688a53893ee29a39c313
MarcoCanha <1636625@hc.ac.je>
2026-03-19 09:30:49 +0000

    fixed ratio from feedback
    remoed mx-50

--- app/index.tsx
+++ app/index.tsx
@@ -6,11 +6,11 @@ export default function HomeScreen() {
   return (
     <View className="bg-background flex flex-1 py-32">
       <View className="text-foreground flex flex-row text-center">HallPass!</View>
-      <View className="border-foreground-transparent mx-50 flex flex-row gap-5 py-4">
-        <View className="flex items-center justify-center">
-          <Checkbox className="border-2"> </Checkbox>
+      <View className="border-foreground-transparent flex flex-row gap-5 py-4">
+        <View className="flex w-16 items-center justify-center">
+          <Checkbox className="border-2"></Checkbox>
         </View>
-        <View className="border-foreground-transparent flex-1 border-b">
+        <View className="border-foreground-transparent flex-1 border-b py-4">
           <Text className="text-foreground">Submit Assignment</Text>
           <Text className="text-foreground-transparent">Due: Oct 20</Text>
         </View>
