791ec3bcb819df3aadc5b33699ab825283a29946
MarcoCanha <1636625@hc.ac.je>
2026-03-19 11:30:48 +0000

    todo item alteration

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,29 @@
+import * as React from 'react';
+import { Checkbox } from '@/components/ui/checkbox';
 import { View } from 'react-native';
 import { Text } from '@/components/ui/text';
-import { Checkbox } from '@/components/ui/checkbox';
 
 export default function HomeScreen() {
+  const [checked, setChecked] = React.useState(true);
+
+  function onCheckedChange() {
+    setChecked(!checked);
+  }
+
   return (
-    <View className="bg-background flex flex-1 py-32">
+    <View className="bg-background flex-1 items-center justify-center gap-5 p-6">
       <View className="text-foreground flex flex-row text-center">HallPass!</View>
-      <View className="border-foreground-transparent flex flex-row gap-5 py-4">
-        <View className="flex w-16 items-center justify-center">
-          <Checkbox className="border-2"></Checkbox>
+      <View className="flex w-full flex-row">
+        <View className="flex h-full w-24 px-8 py-5">
+          <Checkbox
+            className="border-foreground checked:bg-foreground"
+            checked={checked}
+            onCheckedChange={setChecked}
+          />
         </View>
-        <View className="border-foreground-transparent flex-1 border-b py-4">
-          <Text className="text-foreground">Submit Assignment</Text>
-          <Text className="text-foreground-transparent">Due: Oct 20</Text>
+        <View className="border-foreground-transparent flex h-full flex-1 gap-1 border-b py-4">
+          <Text className="text-foreground text-xl">My test item</Text>
+          <Text className="text-foreground-transparent text-xl">Test category</Text>
         </View>
       </View>
     </View>
